fe093d301e4b734949c98b6ca73d7ea24bf3d38d
Dmytro Vovk <dmytro.vovk@dev.pro>
2025-05-02 18:22:12 +0100

    Branching Dev and staging changes

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,6 +1,9 @@
 resource "oci_core_instance" "instance-1" {
-  availability_domain = var.insatnce_availability_domain
+  availability_domain = var.availability_domain
   compartment_id = var.compartment_id
   shape = var.insatnce_shape
 
+  create_vnic_details {
+    subnet_id = oci_core_subnet.subnet.id
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,12 @@
+
+# # OCI - General
+
 variable "region" {
   description = "Default region for OCI resources"
   type        = string
 }
 
-variable "insatnce_availability_domain" {
+variable "availability_domain" {
   description = "Default AD for OCI resources"
   type        = string
 }
@@ -12,6 +15,28 @@ variable "compartment_id" {
   description = "Default compartment for OCI resources"
   type        = string
 }
+
+variable "tenancy_ocid" {
+  description = "OCID of the tenancy"
+  type = string
+}
+
+variable "user_ocid" {
+  description = "OCID of the user calling the API"
+  type = string
+}
+
+variable "private_key_path" {
+  description = "The path (including filename) of the private key stored on the computer"
+  type = string
+}
+
+variable "fingerprint" {
+  description = "Fingerprint for the key pair being used"
+  type = string
+}
+
+# # Compute
 
 variable "insatnce_shape" {
   description = "Default shape for instance resources"
@@ -26,6 +51,24 @@ variable "vcn_cidr" {
 }
 
 variable "subnet_cidr" {
-  description = "The list of one or more IPv4 CIDR blocks for the VCN"
+  description = "The list of one or more IPv4 CIDR blocks for the subnet"
   type        = string
+}
+
+variable "vcn_gateway_enabled" {
+  description = "Enable or disable VCN gateway to Internet"
+  type        = bool
+}
+
+# # LB
+
+variable "lb_displayname" {
+  description = "Display name for Loadbalancer"
+  type        = string
+}
+
+variable "lb_shape" {
+  description = "Shape config for Loadbalancer"
+  type        = string
+  default     = "10Mbps"
 }
